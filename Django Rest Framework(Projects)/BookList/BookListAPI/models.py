91703957c5984809d0c95e6b4b5bb85106cdfeab
Wonder <wonderdermwin@yahoo.com>
2023-01-11 09:53:13 +0000

    Python(Django) Projects

---
+++ Django Rest Framework(Projects)/BookList/BookListAPI/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your Book model here.
+# Create Meta class inside the Book model
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places= 2)
+
+
+    class Meta:
+        indexes = [ 
+                   models.Index(fields=['price']),
+
+        ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books',views.books),
+    # path('books/<int:pk>',views.book),
+]
+
+# Solution code for urls.py (app-level):
+urlpatterns = [
+    path('books',views.books),
+]
